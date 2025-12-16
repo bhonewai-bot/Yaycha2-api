ebb966eef89a1d4e5d7f74bfd94485fc0051c9eb
Bhone Wai <damenaosan@gmail.com>
2025-12-16 22:32:37 +0630

    feat: content, user api

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const { contentRouter } = require("./routers/content");
+const {userRouter} = require("./routers/user");
+app.use("/content", contentRouter);
+app.use("/", userRouter);
+
+app.get("/info", (req, res) => {
+    res.json({ msg: "Yaycha API" });
+});
+
+const server = app.listen(8000,() => {
+    console.log("Yaycha API started at 8000...");
+});
+
+const gracefulShutdown = async () => {
+    await prisma.$disconnect();
+    server.close(() => {
+        console.log("Yaycha API closed.");
+        process.exit(0);
+    });
+};
+
+process.on("SIGTERM", gracefulShutdown);
+process.on("SIGINT", gracefulShutdown);
